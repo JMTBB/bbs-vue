d0a84a4b3447af673a17c5982f9399b1eeb2caaa
LaiJM <2536226456@qq.com>
2019-12-18 21:55:13 +0800

    引入vuetify, 地址写vuetify而不是vuetify/lib

---
+++ src/components/Editor.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col md="2"></v-col>
+            <v-col md="6">
+                <Editor></Editor>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+import Editor from '../components/testpages/Editor'
+export default {    
+    data:() => ({
+        name: 'editors'
+    }),
+    components: {
+        Editor,
+    }
+}
+</script>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-app-bar color="primary" dense>
+      <v-container justify="center">
+        <v-row justify="center">
+          <v-col md="2"></v-col>
+          <v-col md="2">
+            <v-toolbar-title id="logo">NCU BBS</v-toolbar-title>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col md="2" align="center">
+            <v-btn text v-for="item in navItems" :key="item">{{item}}</v-btn>
+          </v-col>
+          <v-col md="2"></v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <Editors></Editors>
+  </div>
+</template>
+
+<script>
+import Editors from '../components/Editor'
+export default {
+  data:() => ({
+    navItems: [
+      "首页",
+      "注册",
+      "登录"
+    ]
+  }),
+  components: {
+    Editors
+  }
+}
+</script>
+
+<style scoped>
+#logo {
+  font-weight: bold;
+  font-size: 30px;
+  font-style: italic;
+  color: #607d8b;
+}
+</style>
